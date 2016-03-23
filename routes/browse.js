e6ada8fa6f41b52f5e15c3a100035a10c7067726
tangyfruits <tangyfruits@gmail.com>
2016-03-23 18:33:50 +1300

    Added RAW/XML

--- routes/browse.js
+++ routes/browse.js
@@ -6,6 +6,7 @@ client.execute("OPEN Colenso");
 var xquery = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';"
 var test = " (//name[@type='place'])[1] "
 var hargod = "declare namespace default ='http://www.tei-c.org/ns/1.0'; for $n in collection('Colenso') let $path := db:path($n) let $name := $n//text() return <result> <path> {$path}</path> <author> {$name}</author></result>"
+
 router.get("/",function(req,res){
   client.execute("XQUERY db:list('Colenso')",
 
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,15 @@ var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0
 var test = " (//name[@type='place'])[1] "
 /* GET home page. */
 router.get("/",function(req,res){
-  client.execute(tei +
-    req.query.search_bar,
+  // client.execute(tei +
+  //   req.query.search_bar,
+  client.execute("XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
+"for $n in (collection('Colenso/Hooker/')//tei:p[position() = 1])" +
+"return db:path($n)",
     function (error, result) {
       if(error){ console.error(error);}
       else {
+        console.log(result.result)
         res.render('index', { title: 'Project: Colenso', place: result.result });
       }
     }
