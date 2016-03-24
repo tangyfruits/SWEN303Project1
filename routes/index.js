3b03ce4f29f29ba9d0641d94cb200688c339a890
Donald Tang <tangyfruits@gmail.com>
2016-03-25 03:05:28 +1300

    Changed the SearchBox. Learnt Bootsrap

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,17 @@ var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';"
+var tei2 = "XQUERY declare element namespace 'http://www.tei-c.org/ns/1.0';"
 var test = " (//name[@type='place'])[1] "
+var test2 = "for $n in (collection('Colenso/Hooker/')//tei:p[position() = 1])  // return db:path($n)"
 /* GET home page. */
 router.get("/",function(req,res){
-  // client.execute(tei +
-  //   req.query.search_bar,
-  client.execute("XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
-"for $n in (collection('Colenso/Hooker/')//tei:p[position() = 1])" +
-"return db:path($n)",
+  client.execute(tei +
+    req.query.search_bar2,
+//   client.execute("XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
+// "for $n in (collection('Colenso/Hooker/')//tei:p[position() = 1])" +
+// "return db:path($n)",
+
     function (error, result) {
       if(error){ console.error(error);}
       else {
